81e0f1518d2c521b53c96eec949cb8f1c611e8af
Dexaf <Federicopisajob@gmail.com>
2021-07-09 11:56:03 +0200

    v1.051
    aggiunte competenze

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-
+        
         gtag('config', 'G-2YG3CMG7ZZ');
     </script>
 
